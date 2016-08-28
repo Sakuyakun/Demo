3f4c7b037198211c88695ba6358683c83cc4037f
sakuya <502566796@qq.com>
2016-08-28 20:44:15 +0800

    update

---
+++ gulpfile.babel.js
@@ -0,0 +1,65 @@
+`use strict`
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins(),
+      PATHS = {
+          dist : './dist/',
+          src : './src/'
+      };
+
+//scss处理
+function gulpStyles(){
+    return () => {
+        gulp.src(PATHS.src + 'scss/**/*.scss')
+            //.pipe($.sass().on('error', sass.logError))
+            .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+            .pipe(gulp.dest(PATHS.dist + 'css'))
+            .pipe($.minifyCss())
+            .pipe($.rename(path => path.basename+='.min'))
+            .pipe(gulp.dest(PATHS.dist + 'css'))
+    }
+}
+
+//js处理
+function gulpScripts(){
+    return () => {
+        gulp.src(PATHS.src + 'js/**/*.js')
+            .pipe($.jshint())
+            .pipe($.jshint.reporter('default'))
+            .pipe($.concat('all.js'))
+            .pipe(gulp.dest(PATHS.dist + 'js'))
+            .pipe($.uglify())
+            .pipe($.rename(path => path.basename+='.min'))
+            .pipe(gulp.dest(PATHS.dist + 'js'))
+    }
+}
+
+//图片处理
+function gulpImg(){
+    return () => {
+        gulp.src(PATHS.src + 'images/**/*')
+            .pipe($.cache($.imagemin({ 
+                optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+                progressive: false, //类型：Boolean 默认：false 无损压缩jpg图片
+                interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+                multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+             })))
+            .pipe(gulp.dest(PATHS.dist + 'images'))
+    }
+}
+
+//监视
+function gulpWatch(){
+    return () => {
+        gulp.watch(PATHS.src + 'scss/**/*.scss', ['styles']);
+        gulp.watch(PATHS.src + 'js/**/*.js', ['scripts']);
+        gulp.watch(PATHS.src + 'imges/**/*', ['images']);
+    }
+}
+
+gulp.task('styles', gulpStyles());
+gulp.task('scripts', gulpScripts());
+gulp.task('images', gulpImg());
+gulp.task('watching', gulpWatch());
