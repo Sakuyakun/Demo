eb6f86098abcc43bc080618f219969ab687fd6cd
Sakuya <sakuyamicahella@gmail.com>
2016-08-29 12:51:57 +0800

    clean & default

---
+++ dist/js/all.js
@@ -0,0 +1,6 @@
+var dom = document.getElementById('container');
+for(var i = 0; i< 4999; i++){
+    console.log(i+i);
+}
+console.log('demo console log message');
+console.log('demo console log message');
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@
 
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
+import del from 'del';
 
 const $ = gulpLoadPlugins(),
       PATHS = {
@@ -12,14 +13,14 @@ const $ = gulpLoadPlugins(),
 //scss处理
 function gulpStyles(){
     return () => {
-        gulp.src(PATHS.src + 'scss/**/*.scss')
+        gulp.src(PATHS.src + 'css/**/*.css')
             //.pipe($.sass().on('error', sass.logError))
             .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
             .pipe(gulp.dest(PATHS.dist + 'css'))
             .pipe($.minifyCss())
             .pipe($.rename(path => path.basename+='.min'))
-            .pipe(gulp.dest(PATHS.dist + 'css'))
-    }
+            .pipe(gulp.dest(PATHS.dist + 'css'));
+    };
 }
 
 //js处理
@@ -32,8 +33,8 @@ function gulpScripts(){
             .pipe(gulp.dest(PATHS.dist + 'js'))
             .pipe($.uglify())
             .pipe($.rename(path => path.basename+='.min'))
-            .pipe(gulp.dest(PATHS.dist + 'js'))
-    }
+            .pipe(gulp.dest(PATHS.dist + 'js'));
+    };
 }
 
 //图片处理
@@ -41,13 +42,13 @@ function gulpImg(){
     return () => {
         gulp.src(PATHS.src + 'images/**/*')
             .pipe($.cache($.imagemin({ 
-                optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+                optimizationLevel: 5,   //类型：Number  默认：3  取值范围：0-7（优化等级）
                 progressive: false, //类型：Boolean 默认：false 无损压缩jpg图片
-                interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-                multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+                interlaced: true,   //类型：Boolean 默认：false 隔行扫描gif进行渲染
+                multipass: true     //类型：Boolean 默认：false 多次优化svg直到完全优化
              })))
-            .pipe(gulp.dest(PATHS.dist + 'images'))
-    }
+            .pipe(gulp.dest(PATHS.dist + 'images'));
+    };
 }
 
 //监视
@@ -56,6 +57,19 @@ function gulpWatch(){
         gulp.watch(PATHS.src + 'scss/**/*.scss', ['styles']);
         gulp.watch(PATHS.src + 'js/**/*.js', ['scripts']);
         gulp.watch(PATHS.src + 'imges/**/*', ['images']);
+    };
+}
+
+//清理目录
+function gulpClean(){
+    return (cb) => {
+        return del([PATHS.dist + 'js', PATHS.dist + 'scss', PATHS.dist + 'css', PATHS.dist + 'images'], cb)
+    }
+}
+
+function gulpDefault(){
+    return () => {
+        gulp.start('styles','scripts','images')
     }
 }
 
@@ -63,3 +77,5 @@ gulp.task('styles', gulpStyles());
 gulp.task('scripts', gulpScripts());
 gulp.task('images', gulpImg());
 gulp.task('watching', gulpWatch());
+gulp.task('clean', gulpClean());
+gulp.task('default', ['clean'], gulpDefault());
