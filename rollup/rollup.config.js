932a80584345bfb4a94fceab2231e092a4124ff5
Saki <sakuyamicahella@qq.com>
2017-09-01 19:38:26 +0800

    Update rollup.config.js

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,10 +1,24 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
+import postcss from 'rollup-plugin-postcss';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
+
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
+
 export default {
   entry: 'src/entry.js',
   format: 'es',
   plugins: [
+    postcss({
+      extensions: [ '.css' ],
+      plugins: [
+        cssnext({ warnForDuplicates: false, }),
+        cssnano()
+      ]
+    }),
     resolve({
       customResolveOptions: {
         moduleDirectory: 'node_modules'
@@ -12,7 +26,8 @@ export default {
     }),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    (process.env.NODE_ENV === 'production' && uglify({}, minify)),
   ],
   external: ['moment'],
   dest: 'yorha.js',
