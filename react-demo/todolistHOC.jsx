fc2749590b92edccca9e97c2d915ec2e81af3bc6
人間開花 <sakuyamicahella@qq.com>
2017-12-09 11:50:39 +0800

    Create todolistHOC.jsx

---
+++ react-demo/todolistHOC.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+const compose = (...fns) => {
+  const start = fns.length - 1;
+  return function(...args) {
+    let i = start;
+    let result = fns[start].apply(this, args);
+    while (i--) result = fns[i].call(this, result);
+    return result;
+  }
+};
+
+// HOC
+const withMaybeNull = isNull => Comp => (props) => {
+  return isNull(props)
+    ? null
+    : <Comp { ...props } />
+}
+const withEither = (conditionalRenderFn, EitherCom) => (Comp) => (props) => {
+  return conditionalRenderFn(props)
+    ? <EitherCom />
+    : <Comp { ...props } />
+}
+
+// Condition
+const isTodosNull = (props) => !props.todos;
+const isTodosEmpty = (props) => !props.todos.length;
+const isTodosLoading = (props) => props.isloading;
+
+// EitherComp
+const emptyTodos = () => {return <div><p>You have no Todos.</p></div>}
+const loadingTodos = () => {return <div><p>Loading todos ...</p></div>}
+
+// Components
+const TodoList = (props) => {
+  const { todos, isloading } = props
+  return (
+    <div>
+      { todos.map(todoItem => <TodoItem key={todoItem.id} todo={todoItem} />) }
+    </div>
+  )
+}
+const TodoItem = (props) => {
+  return <div>todo: {props.todo.content}</div>
+}
+
+// render
+const todosWithHOC = compose(
+  withEither(isTodosLoading, loadingTodos), 
+  withMaybeNull(isTodosNull), 
+  withEither(isTodosEmpty, emptyTodos)
+)
+const TodosRender = todosWithHOC(TodoList)
+
+class App extends Component {
+  state = {
+    todo: [
+      {id: 1, content: 'eat some cake'},
+      {id: 2, content: 'drink some juice'},
+      {id: 3, content: 'take a break'}
+    ],
+    isloading: false
+  }
+  render() {
+    let {isloading, todo} = this.state
+    return (
+      <div>
+        <TodosRender isloading={isloading} todos={todo}></TodosRender>
+      </div>
+    );
+  }
+}
+
+export default App
