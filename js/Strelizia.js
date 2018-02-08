18f18304c34af68a4779f82e567b1fae507002c3
人間開花 <sakuyamicahella@qq.com>
2018-02-08 18:07:12 +0800

    Update Strelizia.js

--- js/Strelizia.js
+++ js/Strelizia.js
@@ -8,7 +8,16 @@ function CheckProgarm(target) {
 }
 
 function MechaCurrDriver (target, name, descriptor) {
-  console.log('connect remote monitor console')
+  const method = descriptor.value
+  const driverStatus = Math.round(Math.random() * 100) ? 'Online' : 'Offline'
+  console.log(`***driver Status: ${driverStatus}***`)
+  descriptor.value = (...args) => {
+    if (driverStatus === 'Online') {
+      method.apply(this, args)
+    } else {
+      console.log('Error, Driver Offline')
+    }
+  }
 }
 
 @CheckProgarm
@@ -25,11 +34,11 @@ class Strelizia {
     console.log(`${this.vice} connected`)
   }
   @MechaCurrDriver
-  useWeapons() {
+  useWeapons(command) {
     return 'init weapons done'
   }
   @MechaCurrDriver
-  useShield() {
+  useShield(command) {
     return 'init shield done'
   }
   systemInstruction (command) {
@@ -44,3 +53,4 @@ class Strelizia {
 
 const BASE = new Strelizia('ZeroTwo', 'Hiro')
 BASE.INIT()
+BASE.useWeapons('spear')
