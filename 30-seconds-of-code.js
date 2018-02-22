7f6fc509e93908c8c0b2489f8b1bba8e91083008
人間開花 <sakuyamicahella@qq.com>
2018-02-22 16:39:46 +0800

    Create 30-seconds-of-code.js

---
+++ 30-seconds-of-code.js
@@ -0,0 +1,34 @@
+// 深合并数组
+const deepFlatten = arr => {
+  return [].concat(...arr.map(v => Array.isArray(v) ? deepFlatten(v) : v));
+};
+
+// 返回两个数组之间的差异值
+const difference = (a, b) => {
+  const b_set = new Set(b)
+  return a.filter(x => !b_set.has(x))
+};
+
+const symmetricDifference = (a, b) => {
+  const set_a = new Set(a);
+  const set_b = new Set(b);
+
+  return [
+    ...a.filter(x => !set_b.has(x)),
+    ...b.filter(x => !set_a.has(x))
+  ]
+}
+
+// 返回数组的所有不同值
+const distinctValuesOfArray = arr => [...new Set(arr)];
+
+// 返回数组中的随机元素
+const sample = arr => arr[Math.floor(Math.random() * arr.length)];
+
+// 随机数组值的顺序
+const shuffle = arr => arr.sort(() => Math.random() - 0.5);
+
+// 返回两个数组中都显示的元素的数组
+const similarity = (arr, values) => arr.filter(v => values.includes(v));
+
+
