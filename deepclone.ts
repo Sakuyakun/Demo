a1f2fbea84ac4c5491d4078e232bb3afa7f65bab
Ve <sakuyamicahella@qq.com>
2022-02-17 14:40:25 +0800

    Create deepclone.ts

---
+++ deepclone.ts
@@ -0,0 +1,32 @@
+function deepClone<T>(obj: T): T;
+function deepClone(obj: any): any {
+  if (obj === null || typeof obj !== 'object') return obj;
+
+  if (obj instanceof Date) {
+    const copy = new Date();
+    copy.setTime(obj.getTime());
+    return copy;
+  }
+
+  if (obj instanceof Array) {
+    const copy = [];
+    for (let i = 0, len = obj.length; i < len; i++) {
+      copy[i] = deepClone(obj[i]);
+    }
+    return copy;
+  }
+
+  if (obj instanceof Object) {
+    const copy: Record<any, any> = {};
+    for (const attr in obj) {
+      if (obj.hasOwnProperty(attr)) {
+        copy[attr] = deepClone(obj[attr]);
+      }
+    }
+    return copy;
+  }
+
+  return obj;
+}
+
+export default deepClone;
