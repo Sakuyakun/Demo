9d275dac1ed25ba3bf8739a1f89d0ead522024a5
Ve <sakuyamicahella@qq.com>
2022-03-06 19:49:44 +0800

    tweak

--- react-boilerplate/app/containers/HomePage/messages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  startProjectHeader: {
-    id: 'boilerplate.containers.HomePage.start_project.header',
-    defaultMessage: '开始你的 Github 项目',
-  }
-});
