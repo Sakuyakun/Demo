fc06a36892dada704d5c86ea46d4216fc0f2c0e1
Ve <bouquetrender@gmail.com>
2022-10-25 14:37:40 +0800

    Update useRowSelection.ts

--- hooks/useRowSelection.ts
+++ hooks/useRowSelection.ts
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import { useDebounceEffect } from 'ahooks'
+import React, { useState, useCallback } from 'react'
 
 declare type Key = React.Key
 type RowKeysWithPageNum = {
@@ -9,6 +10,9 @@ type RowKeysWithPageNum = {
 // selectedRowKeys [1: [rowkey1, rowkey2], 2: [rowkey3]]
 // computedRowKeys [rowkey1, rowkey2, rowkey3]
 export default () => {
+  const [selectedRow, setSelectedRow] = useState<any>({})
+  const [computedRow, setComputedRow] = useState<Key[]>([])
+
   const [selectedRowKeys, setSelectedRowKeys] = useState<RowKeysWithPageNum>({})
   const [computedRowKeys, setComputedRowKeys] = useState<Key[]>([])
 
@@ -16,31 +20,44 @@ export default () => {
     setSelectedRowKeys({})
   }, [])
 
-  const handleRowSelectChange = (rowkeys_curr: Key[], currPage: string) => {
+  const handleRowSelectChange = (rowkeys_curr: Key[], currPage: string, row?: any) => {
     let newObj: RowKeysWithPageNum = {
       ...selectedRowKeys,
     }
     newObj[currPage] = rowkeys_curr
     setSelectedRowKeys(newObj)
+
+    if ((row ?? '') !== '') {
+      let newRowObj = {
+        ...selectedRow,
+      }
+      newRowObj[currPage] = row
+      setSelectedRow(newRowObj)
+    }
   }
 
-  useEffect(() => {
-    let rowkeys_count: Key[] = []
-    let keys = Object.keys(selectedRowKeys)
-    if (keys.length) {
-      keys.forEach((rowkeys: string) => {
-        rowkeys_count = [
-          ...rowkeys_count,
-          ...selectedRowKeys[rowkeys]
-        ]
-      })
-    }
-    setComputedRowKeys(rowkeys_count)
-  }, [selectedRowKeys])
+  useDebounceEffect(
+    () => {
+      let rowkeys_count: Key[] = []
+      let row_count: any = []
+      let keys = Object.keys(selectedRowKeys)
+      if (keys.length) {
+        keys.forEach((rowkeys: string) => {
+          rowkeys_count = [...rowkeys_count, ...selectedRowKeys[rowkeys]]
+          selectedRow[rowkeys] && (row_count = [...row_count, ...selectedRow[rowkeys]])
+        })
+      }
+      setComputedRowKeys(rowkeys_count)
+      setComputedRow(row_count)
+    },
+    [selectedRowKeys],
+    { wait: 0 }
+  )
 
   return {
+    computedRow,
     computedRowKeys,
     handleRowSelectChange,
-    handleCancelAllSelect
+    handleCancelAllSelect,
   }
 }
